c83c7e3a9951135ddc1bdbfeb7bc779711a8945f
Johan Restrepo <johanesteban.restrepo@utp.edu.co>
2021-05-25 15:01:37 -0500

    Backend basico listo

---
+++ models/server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas de la aplicación
+        this.routes();
+    }
+
+    middlewares(){
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y analisis del body
+        this.app.use(express.json());
+
+        // Directoio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el puerto', this.port);
+        });
+    }
+}
+
+module.exports = Server;
